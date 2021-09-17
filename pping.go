dc5b48c02b6d413eea8101dda1be1aa722ad3f31
David Shen <me@pantherman594.com>
2021-09-17 01:18:01 -0400

    Formatting and comments

--- pping.go
+++ pping.go
@@ -2,44 +2,44 @@ package main
 
 import (
 	"fmt"
-	"golang.org/x/net/icmp"
-	"golang.org/x/net/ipv4"
 	"log"
 	"net"
 	"os"
-	"sync"
 	"time"
+
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
 )
 
 var (
 	urls = []string{"google.com", "facebook.com"}
-	matchedUrls []string
-	ipAddrs []*net.IPAddr
-	requests []map[int]time.Time
-	results [][]time.Duration
-	mu sync.Mutex
 )
 
 type Request struct {
-	id int
+	id  int
 	seq int
 }
 
 type Result struct {
-	id int
-	seq int
+	id      int
+	seq     int
 	endTime time.Time
 }
 
 type Error struct {
-	id int
-	seq int
+	id     int
+	seq    int
 	ipAddr *net.IPAddr
-	err error
+	err    error
 }
 
-func ping(conn *icmp.PacketConn, ipAddr *net.IPAddr, id int, seq int, requests chan Request, errors chan Error) {
-	// fmt.Printf("Pinging %s.\n", ipAddr.String())
+// ping sends a ping request through the provided connection to ipAddr, and
+// sends the new request to the requests channel. id and seq are used to clear
+// the request if it fails. Inspired by
+// https://gist.github.com/lmas/c13d1c9de3b2224f9c26435eb56e6ef3
+func ping(conn *icmp.PacketConn, ipAddr *net.IPAddr, id int, seq int,
+	requests chan Request, errors chan Error) {
+	// Create the icmp request.
 	m := icmp.Message{
 		Type: ipv4.ICMPTypeEcho, Code: 0,
 		Body: &icmp.Echo{
@@ -49,13 +49,14 @@ func ping(conn *icmp.PacketConn, ipAddr *net.IPAddr, id int, seq int, requests c
 		},
 	}
 
+	// Marshall it into bytes.
 	b, err := m.Marshal(nil)
 	if err != nil {
 		errors <- Error{id, seq, ipAddr, err}
 		return
 	}
 
-	// Send it
+	// Send it.
 	requests <- Request{id, seq}
 	n, err := conn.WriteTo(b, ipAddr)
 	if err != nil {
@@ -68,21 +69,25 @@ func ping(conn *icmp.PacketConn, ipAddr *net.IPAddr, id int, seq int, requests c
 	}
 }
 
+// listen opens a new PacketConn, listening for ipv4 icmp requests. When
+// received, it parses the request and sends it to the results channel.
 func listen(conn chan *icmp.PacketConn, results chan Result) {
 	c, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer c.Close()
 	conn <- c
-	// defer c.Close()
+	reply := make([]byte, 1500)
 
+	// Listen for packets in an infinite loop.
 	for {
-		reply := make([]byte, 1500)
 		n, peer, err := c.ReadFrom(reply)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			return
 		}
+		endTime := time.Now()
 
 		rm, err := icmp.ParseMessage(1, reply[:n])
 		if err != nil {
@@ -93,7 +98,7 @@ func listen(conn chan *icmp.PacketConn, results chan Result) {
 		case ipv4.ICMPTypeEchoReply:
 			switch pkt := rm.Body.(type) {
 			case *icmp.Echo:
-				endTime := time.Now()
+				// If it is a valid echo packet, send the result to the results channel.
 				results <- Result{pkt.ID, pkt.Seq, endTime}
 			default:
 			}
@@ -103,96 +108,123 @@ func listen(conn chan *icmp.PacketConn, results chan Result) {
 	}
 }
 
-func pinger(conn *icmp.PacketConn, requests chan Request, errors chan Error) {
+func pinger(conn *icmp.PacketConn, ipAddrs []*net.IPAddr, requests chan Request,
+	errors chan Error) {
 	i := int64(0)
 	l := int64(len(ipAddrs))
 
-	// Ping each address in a round-robin fashion
+	// Ping each address in a round-robin fashion.
 	for {
 		id := int(i % l)
 		seq := int(i / l)
+
+		// Send the ping request in a new goroutine.
 		go ping(conn, ipAddrs[id], id, seq, requests, errors)
-		i++
+
 		// Sleep for 1 millisecond so that the listener's buffer isn't overloaded.
-		time.Sleep(1 * time.Millisecond)
+		time.Sleep(time.Millisecond)
+
+		i += 1
 	}
 }
 
-// https://gist.github.com/lmas/c13d1c9de3b2224f9c26435eb56e6ef3
 func main() {
-	ipAddrs = make([]*net.IPAddr, 0, len(urls))
-	matchedUrls = make([]string, 0, len(urls))
+	// Set the cap of ipAddrs and matchedUrls to the length of the provided urls,
+	// so that memory won't need to be reallocated later and because there cannot
+	// be more than the provided urls.
+	ipAddrs := make([]*net.IPAddr, 0, len(urls))
+	matchedUrls := make([]string, 0, len(urls))
 
+	// Create the channels.
 	connReceiver := make(chan *icmp.PacketConn)
 	requestReceiver := make(chan Request)
 	resultReceiver := make(chan Result)
 	errorReceiver := make(chan Error)
 
+	// Start the listener in a goroutine, and store the PacketConn.
 	go listen(connReceiver, resultReceiver)
 	conn := <-connReceiver
 
-	// For each provided url, resolve its ip address and make sure it's pingable
+	// For each provided url, resolve its ip address and make sure it's pingable.
 	for _, url := range urls {
+		// Resolve the IPv4 address.
 		ip, err := net.ResolveIPAddr("ip4", url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Could not get IP for %s: %v\n", url, err)
 		}
 
+		// Attempt to ping the resolved ip address.
 		go ping(conn, ip, 0, 0, requestReceiver, errorReceiver)
 		<-requestReceiver
 
 		select {
 		case <-resultReceiver:
+			// If the ping succeeds, add it to the list of ip addresses and urls.
 			ipAddrs = append(ipAddrs, ip)
 			matchedUrls = append(matchedUrls, url)
-		case e := <- errorReceiver:
-			fmt.Fprintf(os.Stderr, "Failed to ping IP %s for %s: %v\n", ip.String(), url, e.err)
+		case e := <-errorReceiver:
+			fmt.Fprintf(os.Stderr, "Failed to ping IP %s for %s: %v\n",
+				ip.String(), url, e.err)
 		}
 	}
 
+	// Precompute the number of ip addresses, because this will be referenced many
+	// times later.
 	ipCount := len(ipAddrs)
-
-	requests = make([]map[int]time.Time, ipCount)
-	for i, l := 0, ipCount; i < l; i++ {
-		requests[i] = map[int]time.Time{}
-	}
-
-	results = make([][]time.Duration, ipCount)
-	for i, l := 0, ipCount; i < l; i++ {
-		results[i] = []time.Duration{}
-	}
 
 	if ipCount == 0 {
 		log.Fatalf("Unable to find ips for any of the provided urls.\n")
 	}
 
+	// Create the requests and results with the cap ipCount.
+	requests := make([]map[int]time.Time, ipCount)
+	for i, l := 0, ipCount; i < l; i++ {
+		requests[i] = map[int]time.Time{}
+	}
+
+	results := make([][]time.Duration, ipCount)
+	for i, l := 0, ipCount; i < l; i++ {
+		results[i] = []time.Duration{}
+	}
+
 	fmt.Printf("Pinging %d URLs...\n", len(matchedUrls))
 
-	// Start the looped pinger in a separate routine so that we can handle stuff in the main routine.
-	go pinger(conn, requestReceiver, errorReceiver)
+	// Start the looped pinger in a separate routine so that we can handle stuff
+	// in the main routine.
+	go pinger(conn, ipAddrs, requestReceiver, errorReceiver)
 
+	// Listen for requests, results, and errors.
 	for {
 		select {
 		case req := <-requestReceiver:
 			requests[req.id][req.seq] = time.Now()
 		case res := <-resultReceiver:
 			if res.id >= ipCount {
+				fmt.Fprintln(os.Stderr, "Received invalid id.")
 				continue
 			}
 
 			start, ok := requests[res.id][res.seq]
 			if !ok {
+				fmt.Fprintln(os.Stderr,
+					"Response received without a corresponding request.")
 				continue
-				log.Fatal("Response received without a corresponding request.")
 			}
 			delete(requests[res.id], res.seq)
 
+			// Calculate the total duration, log it, and store it in results.
 			dur := res.endTime.Sub(start)
-			fmt.Printf("Pinged %s in %vms.\n", ipAddrs[res.id], float64(dur.Nanoseconds()) / 1000000.0)
+			durMs := float64(dur.Nanoseconds()) / float64(time.Millisecond)
+
+			fmt.Printf("Pinged %s in %0.2fms.\n", ipAddrs[res.id], durMs)
 			results[res.id] = append(results[res.id], dur)
 		case e := <-errorReceiver:
-			delete(requests[e.id], e.seq)
-			fmt.Fprintf(os.Stderr, "Failed to ping IP %s: %v\n", e.ipAddr.String(), e.err)
+			if e.id < ipCount {
+				delete(requests[e.id], e.seq)
+			}
+
+			fmt.Fprintf(os.Stderr, "Failed to ping IP %s: %v\n",
+				e.ipAddr.String(), e.err)
 		}
 	}
 }
